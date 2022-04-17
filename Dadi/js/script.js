eb432b0a0c8b97f5f94d386c0fd7e0c6d70c5195
matte887 <m.erroi92@gmail.com>
2022-04-17 10:32:13 +0200

    Gioco dei dadi

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,1 +1,16 @@
-alert("Hi!");
+// Generare un numero random da 1 a 6 per il PC
+const pcDice = Math.floor(Math.random() * (7 - 1) ) + 1;
+console.log('Giocata del PC:', pcDice);
+
+// Generare un numero random da 1 a 6 per l'utente
+const userDice = Math.floor(Math.random() * (7 - 1) ) + 1;
+console.log("Giocata dell'utente:", userDice);
+
+// Confrontare i due numeri e dichiarare il vincitore
+if (pcDice > userDice) {
+    console.log('Il tuo PC ha vinto!');
+} else if (userDice > pcDice) {
+    console.log('Hai vinto!');
+} else {
+    console.log('Pari');
+}
