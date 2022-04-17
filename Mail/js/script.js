0fc9c4dcfd5d62355f3d1512f1bd433e70f5ca34
matte887 <m.erroi92@gmail.com>
2022-04-17 10:58:15 +0200

    Esercizio mail

---
+++ Mail/js/script.js
@@ -0,0 +1,22 @@
+// Chiedere all'utente la sua mail
+const userMail = prompt ('Scrivi la tua mail');
+console.log("Mail dell'utente:", userMail);
+
+// Creare un array di mail
+const authorizedMails = [
+    'marty.mcfly@bttf.com',
+    'emmet.brown@bttf.com',
+    'biff.tannen@bttf.com',
+    'jennifer.parker@bttf.com'
+]
+
+// Confrontare la mail dell'utente con ciascuna mail nell'array
+for (let i = 0; i < authorizedMails.length; i++) {
+    if (userMail === authorizedMails[i]) {
+        console.log('Accesso consentito');
+    } else {
+        console.log('Accesso negato');
+    }
+}
+
+// Stampare in console un messaggio adeguato
